aed67fc9951e810c5aec53d2776644ffebed15b2
Codeco-Sobirjon <sobirbobojonov05763104@gmail.com>
2025-01-06 21:14:22 +0500

    changed

--- config/settings/base.py
+++ config/settings/base.py
@@ -97,7 +97,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'var/www/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'var/www/Social-Media-/media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
